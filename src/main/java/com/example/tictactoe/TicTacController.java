6e0ce35f85d0a8465c54b016982057a235b44cb1
fksxiz <malyshkin_04@bk.ru>
2024-02-15 22:23:54 +0300

    working tik tak toe

--- src/main/java/com/example/tictactoe/TicTacController.java
+++ src/main/java/com/example/tictactoe/TicTacController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -17,23 +18,42 @@ public class TicTacController {
     @FXML
     private Label resultText;
     @FXML
+    private TextField player1;
+    @FXML
+    private TextField player2;
+    @FXML
     private Canvas TicTacCanvas;
 
     private enum Symbol{
-        empty,
         o,
         x
     }
 
+    private enum State{
+        vertical,
+        horizontal,
+        diagonalLR,
+        diagonalRL
+    }
+
     private Symbol[][] Matrix = new Symbol[3][3];
 
-    private boolean isX=true;
+    private boolean isX;
+    private boolean isEnded;
+    private int c;
+    private double offset;
+
+    private final double lineOffset = 5;
 
     @FXML
     public void initialize() {
         //Установка обработчика событий
         TicTacCanvas.setOnMouseClicked(this::canvasMouseClick);
         TicTacCanvas.setHeight(TicTacCanvas.getWidth());
+        isEnded=false;
+        isX=true;
+        c=0;
+        offset = TicTacCanvas.getWidth()/3;
         drawField();
     }
 
@@ -52,8 +72,6 @@ public class TicTacController {
 
     private void drawXO(int row, int col){
         GraphicsContext gc = TicTacCanvas.getGraphicsContext2D();
-        double offset = TicTacCanvas.getWidth()/3;
-        double lineOffset = 5;
         if(Matrix[row][col]==null) {
             if (this.isX) {
                 gc.setStroke(Color.RED);
@@ -65,7 +83,68 @@ public class TicTacController {
                 gc.strokeOval(col * offset + lineOffset, row * offset + lineOffset, offset - lineOffset * 2, offset - lineOffset * 2);
                 Matrix[row][col]=Symbol.o;
             }
+            c++;
+            checkLines(isX?Symbol.x:Symbol.o);
             isX=!isX;
+        }
+    }
+
+    private void checkLines(Symbol s){
+        //Проверка вертикальных и горизонтальных линий
+        for(int i =0;i<3;i++){
+            if((Matrix[i][0]==s&&Matrix[i][1]==s&&Matrix[i][2]==s)){
+                resultText.setText("Победил "+(s==Symbol.x?player1.getText():player2.getText())+"!");
+                isEnded=true;
+                drawLine(i,0,i,2,State.horizontal);
+                return;
+            }else {
+                if ((Matrix[0][i] == s && Matrix[1][i] == s && Matrix[2][i] == s)) {
+                    resultText.setText("Победил " + (s == Symbol.x ? player1.getText() : player2.getText()) + "!");
+                    isEnded = true;
+                    drawLine(0,i,2,i,State.vertical);
+                    return;
+                }
+            }
+        }
+
+        //Проверка диагональных линий
+        if((Matrix[0][0]==s&&Matrix[1][1]==s&&Matrix[2][2]==s)){
+            resultText.setText("Победил "+(s==Symbol.x?player1.getText():player2.getText())+"!");
+            isEnded=true;
+            drawLine(0,0,2,2,State.diagonalLR);
+            return;
+        }else {
+            if ((Matrix[0][2] == s && Matrix[1][1] == s && Matrix[2][0] == s)) {
+                resultText.setText("Победил " + (s == Symbol.x ? player1.getText() : player2.getText()) + "!");
+                isEnded = true;
+                drawLine(0,2,2,0,State.diagonalRL);
+                return;
+            }
+        }
+
+        //Проверка на ничью
+        if(c==9){
+            resultText.setText("Ничья");
+            isEnded=true;
+        }
+    }
+
+    private void drawLine(int r1, int c1, int r2, int c2, State s ){
+        GraphicsContext gc = TicTacCanvas.getGraphicsContext2D();
+        gc.setStroke(Color.DARKBLUE);
+        switch(s.ordinal()){
+            case 0:
+                gc.strokeLine(offset * c1+offset/2 , offset * r1 , offset * c2+offset/2, offset * r2+offset);
+                break;
+            case 1:
+                gc.strokeLine(offset * c1 , offset * r1+offset/2 , offset * c2+offset, offset * r2+offset/2);
+                break;
+            case 2:
+                gc.strokeLine(offset * c1 , offset * r1 , offset + offset * c2, offset + offset * r2);
+                break;
+            case 3:
+                gc.strokeLine(offset + offset * c1, offset * r1, offset * c2, offset+offset * r2);
+                break;
         }
     }
 
@@ -74,23 +153,28 @@ public class TicTacController {
         drawField();
         resultText.setText("-");
         Matrix = new Symbol[3][3];
+        isEnded=false;
+        isX=true;
+        c=0;
     }
 
     private void canvasMouseClick(MouseEvent event){
-        if(event.getButton()== MouseButton.PRIMARY){
-            double offset = TicTacCanvas.getWidth()/3;
-            double x = event.getX();
-            double y = event.getY();
-            int r=-1,c=-1;
-                for(int i=0;i<3;i++) {
-                    if(x>=i*offset&&x<i*offset+offset) {
-                        c=i;
+        if(event.getButton()== MouseButton.PRIMARY) {
+            if (!isEnded) {
+                double offset = TicTacCanvas.getWidth() / 3;
+                double x = event.getX();
+                double y = event.getY();
+                int r = -1, c = -1;
+                for (int i = 0; i < 3; i++) {
+                    if (x >= i * offset && x < i * offset + offset) {
+                        c = i;
                     }
-                    if(y>=i*offset&&y<i*offset+offset) {
-                        r=i;
+                    if (y >= i * offset && y < i * offset + offset) {
+                        r = i;
                     }
                 }
-                drawXO(r,c);
+                drawXO(r, c);
             }
         }
+    }
 }
